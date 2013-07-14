2b0bf4b9f390610d3f4c9643042507b96de9fa2f
Javier Arguello <jarguellososa@gmail.com>
2013-07-14 15:40:28 -0430

    Archivo: replica.pl Copie todos los metodos del archivo getFile en el archivo replica.pl

--- replica.pl
+++ replica.pl
@@ -5,7 +5,6 @@ package replica;
 use lib qw(.);
 use lib qw(./lib/perl5/site_perl/5.12.4/);
 use lib qw(./lib/lib/perl5/site_perl/5.12.4);
-#use lib qw(./lib/lib/perl5/site_perl/5.12.4/darwin-thread-multi-2level);
 use utf8;
 use strict;
 use threads;
@@ -22,7 +21,10 @@ use Data::Dumper;
 
 use Archivo;
 use InfoNodo;
+use Net::SFTP::Foreign;
+use Digest::MD5;
 
+use constant K => 1; 
 use constant LOG            => 1;
 use constant DEBUG          => 1;
 use constant MC_DESTINATION => '226.1.1.4:2000';
@@ -36,6 +38,7 @@ use constant REP_RPC_PORT => '8082';
 
 #   Variables globales de un servidor replica
 my $coord      :shared;
+my $raiz     = '/tmp';
 my %tablaNodos :shared;
 my %pidRep     :shared;
 my $hostname = `hostname`;
@@ -205,9 +208,14 @@ sub agregarServidor {
     }
 }
 
-#   
+# Rutina que crea el directorio raiz
+sub crearRaiz{
+    mkdir $raiz;
+}
+# Rutina que borra todos los archivos almacenados en la replica
 sub wipe {
-    #body ...
+    system("rm -r $raiz/*i");
+    &crearRaiz;
 }
 
 #   Metodo que monitorea el estado de los servidores replica del sistema
@@ -260,9 +268,8 @@ sub getTabla {
     print Dumper (\%tablaNodos) if DEBUG;
 
     &getTabla() unless exists $tablaNodos{"$my_pid"};
-
-    #wipe($my_pid) if $tablaNodos{$my_pid}->contar_archivos() == 0;
-
+    &crearRaiz;
+    wipe($my_pid) if $tablaNodos{$my_pid}->contar_archivos() == 0;
     1;
 }
 
@@ -387,6 +394,204 @@ sub fromStr2Tabla {
         $result{$pid} = $nodo;
     }
     %result;
+}
+
+#
+#
+sub commit {
+    my $archivo = shift;
+    my $checksum = &checksum($archivo);
+    my ($version, $checksumL,@replicas) = &getRep($archivo);
+    
+    if ($checksum ne $checksumL){
+        &send2rep($archivo,$version+1,@replicas);
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+#
+#
+sub pull{
+    my $archivo = shift;
+    my $version = shift;
+    my $checkL;
+    my $checkR;
+    my @arreglar;
+    my $rep;
+
+    if (defined($version)){
+        print "error\n" unless (&versionOK($archivo,$version));
+    }else{
+        $version = &getVersion($archivo);
+    }
+
+    ($checkR,$rep,@arreglar) = &validarChecksum($archivo,$version);
+    while ($checkR ne $checkL){
+        &getFromRep($archivo,$version,$rep);
+        $checkL = &checksum($archivo);
+    }
+    &arreglarRep($archivo,$version,@arreglar) if (@arreglar);
+}
+
+sub arreglarRep{
+    my $archivo = shift;
+    my $version = shift;
+    my @replicas = @_;
+
+    &send2rep($archivo,$version,@replicas);
+
+}
+# Rutina que calcula el checksum de un archivo
+sub checksum{
+    my $archivo = shift;
+    my $checksum;
+    eval{
+        open(FILE, "/tmp/$archivo") or die "Can't find file $archivo\n";
+        my $ctx = Digest::MD5->new;
+        $ctx->addfile(*FILE);
+        $checksum = $ctx->hexdigest;
+        close(FILE);
+    }; 
+    return $checksum;
+}
+
+# Rutina que valida los checksum del archivo en todas las replicas
+# Retorna el checksum valido y la replica a solicitar
+# Si existe un error en alguno de los checksum reenvia el archivo a 
+# la replica con el checksum incorrecto
+sub validarChecksum{
+    my $archivo = shift;
+    my $version = shift;
+    my ($v,$c,@replicas) =  &getRep($archivo);
+    my %checksums;
+    shift @replicas;
+    foreach(@replicas){
+        my $rep_url = "http://$_:" . REP_PRC_PORT . '/RPC2';
+        my $rep = Frontier::Client->new(url => $rep_url);
+        my $result = $rep->call('rep.checksum',$archivo,$version);
+        push (@{$checksums{$result}},$_);
+    }
+
+    my @modaCheck;
+    my $moda;
+    my @arreglar;
+    while(my($check,@rep) = each %checksums){
+        if (@modaCheck < @rep){
+            if (@modaCheck){
+                push(@arreglar,$_) foreach @modaCheck;
+            }
+            ($moda,@modaCheck) = ($check,@rep);
+        }
+    }
+    return($moda,$modaCheck[0],@arreglar);
+}
+
+sub notificarCommit{
+    my $archivo = shift;
+    my $version = shift;
+    my @reps = @_;
+    
+    print "Notificando commit al grupo multicast\n" if DEBUG;
+    my $socket = IO::Socket::Multicast->new(PeerAddr=>MC_DESTINATION);
+    my $datos  = "2,";
+    $datos .= $archivo . ",";
+    $datos .= $version . ",";
+    $datos .= "@reps";
+    $socket->send($datos) || die "No se pudo notificar al grupo: $!";
+    print "Notificacion enviada al grupo multicast\n" if DEBUG;
+}
+
+# Rutina que envia un archivo a la replica
+# Parametros
+# @servidores: lista de los servidores a los que se enviara el archivo
+# $archivo: achivo que se enviara
+sub send2rep{
+    my $archivo = shift;
+    my $version = shift;
+    my @reps = @_;
+    my @pids;
+    foreach(@reps){
+        print "Enviando $archivo a $_";
+        my $host = $_;
+        my $sftp = Net::SFTP::Foreign->new(host=>$_, user=>'javier');
+        $sftp->mkdir($raiz/$archivo);
+        $sftp->put("/tmp/$archivo","$raiz/$archivo/$version");
+        push(@pids,$pidRep{$_});
+    }
+    #   NOTIFICAR A TODO EL MUNDO EL ENVIO DE ARCHIVOS
+    #   PARA QUE ACTUALICEN SU TABLA
+    my $checksum = &checksum($archivo);
+    &notificarCommit($archivo,$version,$checksum,@pids);
+}
+
+# Rutina que recibe un archivo de una replica
+# Parametros
+# $archivo
+# $version (la ultima por defecto)
+# $replica
+sub getFromRep{
+    my $archivo = shift;
+    my $rep = shift;
+    my $version = shift;
+    $version = &getVersion($archivo) unless defined($version);
+    my $sftp = Net::SFTP::Foreign->new(host=>$rep, user=>'javier');
+    $sftp->get("$raiz/$archivo/$version","/tmp/$archivo");
+
+}
+
+# Rutina que busca las replicas que tienen un archivo dado y la ultima version
+# disponible 
+sub getRep{
+    my $archivo = shift;
+    my $version = 0;
+    my @replicas;
+    my $checksum;
+    while (my($pid, $rep) = each %tablaNodos) {
+        my $arch = $rep->get($archivo); 
+        if (defined($arch)){
+            push(@replicas,$rep->nombre);
+            $version = $arch->contar_versiones;
+            $checksum = $arch->get_version($version);
+        }
+        @replicas = &lowRep if (!@replicas);
+        return ($version,$checksum,@replicas);
+    }
+}
+
+# Rutina que busca las k replicas con menos carga
+#
+sub lowRep{
+    my %cp;
+    my @replicas;
+    while (my($pid, $rep) = each %tablaNodos) {
+        push (@{$cp{$rep->contar_archivos}},$rep->nombre);
+    }  
+    my @cargas = (sort keys %cp);
+    
+    my $krep = 0;
+    my $key = shift @cargas;
+    while ($krep < K){
+        $key = shift @cargas unless ($cp{$key});
+        push(@replicas, shift @{$cp{$key}});
+        $krep++;
+    }
+    return @replicas;
+}
+
+# Rutina que retorna la ultima version de un archivo
+sub getVersion{
+    my $archivo = shift;
+    my $version;
+    while (my($pid, $rep) = each %tablaNodos) {
+        my $arch = $rep->buscar_archivo($archivo); 
+        if (defined($arch)){
+            $version = $arch->contar_versiones();
+            last;
+        }
+        return $version;
+    }
 }
 
 ############
